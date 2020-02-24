23addb1f117a0b4311da444514729d5d817551a0
Ryan Lemos <ryanlucas.lemos@gmail.com>
2020-02-24 15:39:17 -0300

    Inclui o efeito de flip inicial

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import PropTypes from 'prop-types';
-import { Container } from './styles';
+import {
+  Container, CardBack, CardFront, InnerContainer,
+} from './styles';
 
 
 export default function Card({ data, changeOppened, index }) {
+  const ref = useRef(null);
   function openCard() {
     changeOppened(index);
   }
+
+
   return (
-    <Container onClick={openCard} data={data}>
-      {data.oppened
-        && <data.component size="5em" />}
+    <Container onClick={openCard} ref={ref}>
+      <InnerContainer data={data}>
+        <CardFront />
+        <CardBack>
+          <data.component size="5em" />
+        </CardBack>
+      </InnerContainer>
+
     </Container>
   );
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,15 +1,47 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+
+  background-color: transparent;
+  perspective: 1000px;
+
   border: none;
-  background: ${(props) => (props.data.oppened ? '#f18867' : '#50bda1')};
   display: flex;
   align-items: center;
   justify-content: center;
-  color: #fff;
   border-radius: 4px;
   box-shadow: 0 0 14px 0 rgba(0, 0, 0, 0.02);
+  color: #fff;
   margin-bottom: 10px;
   height: 15vh;
   width: 15vw;
+`;
+
+export const InnerContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+  ${(props) => (props.data.oppened ? 'transform: rotateY(180deg);' : '')}
+`;
+
+export const CardSides = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+`;
+
+export const CardFront = styled(CardSides)`
+  background: #50bda1;
+`;
+
+export const CardBack = styled(CardSides)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f18867;
+  transform: rotateY(180deg);
 `;
