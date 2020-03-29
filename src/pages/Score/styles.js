8bdbf0b5320eb779ee595e4b69cd8dec454acfbe
Ryan Lemos <ryanlucas.lemos@gmail.com>
2020-03-29 18:25:27 -0300

    Corrige design no mobile

--- src/pages/Score/styles.js
+++ src/pages/Score/styles.js
@@ -4,15 +4,16 @@ import { Container as StartContainer, ContentBox } from '../Start/styles';
 
 
 export const Container = styled(StartContainer)`
-    
 `;
 
 export const Content = styled(ContentBox)`
   height: auto;
-  padding: 40px;
+  width: auto;
+  min-width: 50vw;
+  padding: 40px 20px;
+
   div + div {
-    padding-left:15px;
-    padding-bottom: 20px; 
+    padding: 20px 15px; 
     margin-left: 15px;
     border-left: 2px solid #949494;
   }
@@ -21,36 +22,34 @@ export const Content = styled(ContentBox)`
 export const Grid = styled.div`
   flex-direction: column;
   height: auto;
-
-
 `;
 
 export const Title = styled.h2`
   margin-bottom: 15px;
-  font-size: 35px;
+  font-size: 25px;
   color: #f18867;
-
 `;
 
 export const Line = styled.hr`
   background: #515151;
-  width: 20vw;
+
+  min-width: 20vw;
   height: 3px;
   margin-top: 10px;
   margin-bottom: 20px;
 `;
 export const FinalResult = styled(Title)`
-  font-size: 28px;
+  font-size: 23px;
 `;
 
 export const ScoreText = styled.h3`
-  font-size: 25px;
+  font-size: 20px;
   margin-top: 10px;
   margin-bottom: 10px;
   color: #50bda1;
   span {
     margin-left: 5px;
-    font-size: 20px;
+    font-size: 15px;
     color: #515151;
     font-weight: bold;
   }
